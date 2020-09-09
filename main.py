06458d1163ac2fe03e74b793569afbc9cb8ffe16
sethstenford1983 <70973790+sethstenford1983@users.noreply.github.com>
2020-09-09 11:16:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import re
+
+def fn(a):
+    y = re.match("^\d+\-\d+\-\d+\s\d+\:(\d+)\:\d+$",a)
+    if y is None:
+        print('не понимаю')
+    else:
+        t = y.groups()
+        return t[0]
+print(fn("2018-01-09 12:32:04"))
+
+
